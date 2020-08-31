e0a09247e791707ac0c14b783d6e87ca6e6da21a
h4ppyy <93immm@naver.com>
2020-08-31 17:59:03 +0900

    global logger

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,20 +1,23 @@
-import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import { all, fork, call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 
 const delay = (ms) => new Promise(res => setTimeout(res, ms))
 
-function* sagaInc(action) {
-  yield call(() => { console.log('sagaInc') })
+function* logging(action) {
+  yield fork(() => { console.log('logging -> ', action) })
+}
+
+function* sagaInc() {
   yield delay(1000)
   yield put({ type: "INCREMENT" })
 }
 
-function* sagaDec(action) {
-  yield call(() => { console.log('sagaDec') })
+function* sagaDec() {
   yield delay(1000)
   yield put({ type: "DECREMENT" })
 }
 
 export function* mySaga() {
   yield takeEvery("SAGA_INCREMENT", sagaInc)
-  yield takeEvery("SAGA_DECREMENT", sagaDec)
+  yield takeLatest("SAGA_DECREMENT", sagaDec)
+  yield takeEvery("*", logging)
 }
